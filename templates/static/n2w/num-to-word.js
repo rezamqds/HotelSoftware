ce2b2b53eb86d3b988dd3ba20114d79ecc066fad
rezamqds <rezamqds@gmail.com>
2023-09-23 14:21:22 +0330

    added and fixed xD

--- templates/static/n2w/num-to-word.js
+++ templates/static/n2w/num-to-word.js
@@ -1,1 +1,7 @@
-function calc(){var n=checkNumberToEn($("#number").val().split(",").join(""));if(n){var a=Num2persian(n);$("#word").text(a),$("#show").removeClass("d-none")}}
+function calc() {
+  var n = checkNumberToEn($("#number").val().split(",").join(""));
+  if (n) {
+    var a = Num2persian(n);
+    $("#word").text(a+" تومان "), $("#show").removeClass("d-none");
+  }
+}
