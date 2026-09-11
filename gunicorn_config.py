31ec5212b8038794bb1fa467fafbb8e61ab0907a
rezamqds <rezamqds@gmail.com>
2026-09-11 13:46:56 +0330

    gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,6 @@
-workers = 2  # Adjust this based on your server's resources
+# Gunicorn configuration
+# NOTE: On Windows it is recommended to run via `start.bat` or `python run.py`
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = '0.0.0.0:7331'  # Replace with your desired IP address and port
